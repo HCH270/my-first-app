108d770b318d848b6724e3c42b7a08b88ba051d8
HCH270 <hchawkins@gmail.com>
2021-05-06 14:57:34 -0500

    Directive Deep Dive lesson
    BasicHighlightDirective
    BetterHighlightDirective
    UnlessDirective
    
    Started by moving Max's src zip into my first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
-  }
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2,4];
+  onlyOdd = false;
+  value = 10;
 }
